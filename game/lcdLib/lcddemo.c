ae8305ae2c50d7d8902c757e390a10e8ccd93dee
carlosmontgomery <carlosmontgomery@github.com>
2019-11-06 17:19:32 -0700

    Created a filled in square and a filled in right triangle.

---
+++ game/lcdLib/lcddemo.c
@@ -0,0 +1,46 @@
+/** \file lcddemo.c
+ *  \brief A simple demo that draws a string and circle
+ */
+
+#include <libTimer.h>
+#include "lcdutils.h"
+#include "lcddraw.h"
+#include <stdio.h>
+
+/** Initializes everything, clears the screen, draws "hello" and the circle */
+int
+main()
+{
+  configureClocks();
+  lcd_init();
+  u_char width = screenWidth, height = screenHeight;
+
+  clearScreen(COLOR_BLACK);
+  draw_triangle();
+   
+  
+  // drawString5x7(20,20, "hello", COLOR_GREEN, COLOR_RED);
+
+  // fillRectangle(30,30, 60, 60, COLOR_ORANGE);
+  
+}
+void draw_asterisk(int x, int y, int color){
+   for(int i = 0; i< 80;i++){
+     drawPixel(i,i,COLOR_GOLD);}
+   for(int j = 0; j<80;j++){
+     drawPixel(j,40,COLOR_GOLD);
+     }  
+}
+
+void draw_square(){ // Draws a square that is filled in. Color : COLOR_GOLD.
+   for(int i = 80; i > 0;i--)
+    for(int j = 0; j < 80; j++)
+      drawPixel(i,j,COLOR_GOLD);
+}
+
+void draw_triangle(){ //Draw a right triangle size of 80 pixels. Color: COLOR_GOLD.
+    for(int i = 0; i < 80;i++)
+      for(int j = 0; j < (i+1); j++)
+	drawPixel(j,i,COLOR_GOLD);
+
+}
